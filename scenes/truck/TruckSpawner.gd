c7dfb17b481eb02c4aefb13c58d9edacb4a925d4
Lewis McNeill <6698050+lewis785@users.noreply.github.com>
2023-07-08 00:38:02 +0100

    ADD truck spawning to map

--- scenes/truck/TruckSpawner.gd
+++ scenes/truck/TruckSpawner.gd
@@ -1,6 +1,13 @@
 extends Area2D
 
 @export var truck_scene : PackedScene
+@export var collision: CollisionPolygon2D
+
+func _ready():
+	if collision:
+		$Highlight.polygon = collision.polygon
+		$Highlight.position = collision.position
+	
 
 func _input_event( viewport, event, shape_idx):
 	if (!event is InputEventMouseButton || !event.pressed):
@@ -8,3 +15,10 @@ func _input_event( viewport, event, shape_idx):
 	
 	var truck = truck_scene.instantiate()
 	add_child(truck)
+
+
+func _on_mouse_entered():
+	$Highlight.show()
+
+func _on_mouse_exited():
+	$Highlight.hide()
