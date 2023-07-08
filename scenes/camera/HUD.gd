978dcbc426a403980d51c751fe50dddc8da255ca
Nicholas Robinson <kylenr23@gmail.com>
2023-07-08 17:58:12 +0100

    ADD Time and Score Labels

---
+++ scenes/camera/HUD.gd
@@ -0,0 +1,33 @@
+extends CanvasLayer
+
+@export var camera: Camera2D
+#@export var tc: TimerComponent
+@export var score: ScoreComponent
+
+var score_label: Label
+var time_label: Label
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	score_label = $ScoreLabel
+	time_label  = $TimeLabel
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	# TODO: Get Score
+	update_score_label(10)
+	# TODO: Get Time Left
+	update_time_label(245)
+
+func update_score_label(score):
+	score_label.text = str(score)
+
+func update_time_label(time):
+	time_label.text = time_to_text(time)
+
+# Converts number of seconds to M:SS format
+func time_to_text(time):
+	var minutes = "%02d" % floor(time/60)
+	var seconds = "%02d" % (time%60)
+	return minutes + ":" + seconds
