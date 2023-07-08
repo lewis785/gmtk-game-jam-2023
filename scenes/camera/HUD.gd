66c95ff8abdc4394125afa9fa9e29fe39aee705c
Nicholas Robinson <kylenr23@gmail.com>
2023-07-08 18:51:26 +0100

    ADD Score on HUD through Signal Bus

--- scenes/camera/HUD.gd
+++ scenes/camera/HUD.gd
@@ -2,13 +2,17 @@ extends CanvasLayer
 
 @export var camera: Camera2D
 #@export var tc: TimerComponent
-@export var score: ScoreComponent
+#@export var score: ScoreComponent
+var score = 0
 
+var signal_bus
 var score_label: Label
 var time_label: Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	signal_bus = get_node("/root/SignalBus")
+	signal_bus.connect('score_update', update_score)
 	score_label = $ScoreLabel
 	time_label  = $TimeLabel
 
@@ -16,9 +20,13 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	# TODO: Get Score
-	update_score_label(10)
+	#update_score_label(10)
 	# TODO: Get Time Left
 	update_time_label(245)
+	
+func update_score(points):
+	score += points
+	update_score_label(score)
 
 func update_score_label(score):
 	score_label.text = str(score)
