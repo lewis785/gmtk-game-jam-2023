1762acba10905d6eef8395091eff2f8983c1fd22
Nicholas Robinson <kylenr23@gmail.com>
2023-07-08 01:20:08 +0100

    FIX Person Hitbox and Move Component CB

--- scripts/MoveComponent.gd
+++ scripts/MoveComponent.gd
@@ -3,7 +3,6 @@ extends CharacterBody2D
 @export var speed = 1000.0
 @export_enum("UP", "RIGHT") var direction = "UP"
 @export var animation: AnimationPlayer
-@export var cb: CharacterBody2D
 
 var movementDirection = {
 	"UP": Vector2.UP,
@@ -14,7 +13,7 @@ func move(delta: float):
 	velocity = movementDirection[direction] * speed * delta
 	if animation != null:
 		animation.play("default")
-	cbmove_and_slide()
+	move_and_slide()
 
 func _physics_process(delta):
 	move(delta)
