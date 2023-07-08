ee5f34c970a083d628444c721e86fd03feddaece
Lewis McNeill <6698050+lewis785@users.noreply.github.com>
2023-07-08 13:51:45 +0100

    ADD spawning frogs on cross spawner

--- scenes/CrosserSpawn.gd
+++ scenes/CrosserSpawn.gd
@@ -2,13 +2,20 @@ extends Area2D
 
 @export var crossers: Array[PackedScene]
 @export_range(0.0, 1.0, 0.1) var spawn_change
+@export_range(0.0, 1.0, 0.1) var frog_spawn_rate = 0.2
 
 var min_spacing = 5
 var time_since_last = 5
 var rng = RandomNumberGenerator.new()
 
+func type_of_crosser_to_spawn():
+	var random_number = rng.randf_range(0.0, 1.0)
+	if random_number < frog_spawn_rate:
+		return crossers[1].instantiate()
+	return crossers[0].instantiate()
+
 func spawn_crosser():
-	var crosser = crossers[0].instantiate()
+	var crosser = type_of_crosser_to_spawn()
 	crosser.position = crosser.position - Vector2(5, -4)
 	var tween = get_tree().create_tween()
 	tween.parallel().tween_property(crosser, "scale", Vector2(1.5, 1.5), 1)
