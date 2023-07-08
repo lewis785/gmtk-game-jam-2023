7f705d08a8b65430f7af1c6684a7ab3289546228
Nicholas Robinson <kylenr23@gmail.com>
2023-07-08 17:00:59 +0100

    FIX Remove Camera debug

--- scenes/camera/Camera.gd
+++ scenes/camera/Camera.gd
@@ -4,12 +4,11 @@ extends Camera2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var x = map.get_window().size.x
-	var y = map.get_window().size.y
-	print(x)
-	print(y)
-	self.limit_right = x
-	self.limit_bottom = y
+	if map != null:
+		var x = map.get_window().size.x
+		var y = map.get_window().size.y
+		self.limit_right = x
+		self.limit_bottom = y
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
