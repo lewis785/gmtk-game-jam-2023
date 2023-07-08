aa9a45d1eaaad075aa0cb6ca46fb728a65a4ef98
Lewis McNeill <6698050+lewis785@users.noreply.github.com>
2023-07-08 17:39:39 +0100

    FIX vehicles being able to be spawned inside other vehicles

--- scenes/truck/TruckSpawner.gd
+++ scenes/truck/TruckSpawner.gd
@@ -3,12 +3,25 @@ extends Area2D
 @export var truck_scene : PackedScene
 @export var collision: CollisionPolygon2D
 @export var score: ScoreComponent
+@export var hover_color: Color
+@export var blocked_spawn_color: Color
+
+var can_spawn = true
 
 func _ready():
+	allow_spawning()
 	if collision:
 		$Highlight.polygon = collision.polygon
 		$Highlight.position = collision.position
 	
+func allow_spawning():
+	can_spawn = true
+	$Highlight.color = hover_color
+	
+func disable_spawning():
+	can_spawn = false
+	$Highlight.color = blocked_spawn_color
+
 func is_left_click(event):
 	if (!event is InputEventMouseButton || !event.pressed):
 		return false
@@ -16,7 +29,8 @@ func is_left_click(event):
 	return event.button_index == MOUSE_BUTTON_LEFT
 
 func _input_event( viewport, event, shape_idx):
-	if (is_left_click(event)):		
+	if (is_left_click(event) && can_spawn):		
+		disable_spawning()
 		var truck = truck_scene.instantiate()
 		add_child(truck)
 
@@ -34,4 +48,7 @@ func _on_body_exited(body):
 	if body.is_in_group("Vehicle"):
 		increase_score(body.score_value)
 		body.queue_free()
-		
+
+func _on_vehicle_spawn_area_body_exited(body):
+	if body.is_in_group("Vehicle"):
+		allow_spawning()
