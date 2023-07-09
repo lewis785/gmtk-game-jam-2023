9132eca04157c9c49753856e33fe8f30669b2f36
Lewis McNeill <6698050+lewis785@users.noreply.github.com>
2023-07-09 14:53:54 +0100

    ADD how to play screen

--- scenes/ui/HowToPlay.gd
+++ scenes/ui/HowToPlay.gd
@@ -2,6 +2,3 @@ extends CanvasLayer
 
 func _on_start_pressed():
 	get_tree().change_scene_to_file("res://scenes/world.tscn")
-
-func _on_how_to_play_pressed():
-	get_tree().change_scene_to_file("res://scenes/ui/HowToPlay.tscn")
