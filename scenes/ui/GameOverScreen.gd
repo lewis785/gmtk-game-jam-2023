61ab421e645b3281d5111f4706b123378d05e1fa
Nicholas Robinson <kylenr23@gmail.com>
2023-07-09 14:38:42 +0100

    CHANGE Remove Quit button

--- scenes/ui/GameOverScreen.gd
+++ scenes/ui/GameOverScreen.gd
@@ -5,9 +5,6 @@ extends CanvasLayer
 func _on_restart_pressed():
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://scenes/world.tscn")
-
-func _on_quit_pressed():
-	get_tree().quit()
 	
 func set_score(score):
 	var score_label = $PanelContainer/MarginContainer/Rows/VBoxContainer/ScoreValue
