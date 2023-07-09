2fa77cab1230e6c4640c77f57e578eacea322a5a
Lewis McNeill <6698050+lewis785@users.noreply.github.com>
2023-07-09 03:32:29 +0100

    REFACTOR make frog script run from top of the scene

--- scenes/person/Frog.gd
+++ scenes/person/Frog.gd
@@ -1,5 +1,12 @@
 extends MovementComponent
 
+@export var score_comp: ScoreComponent
+@export var collision_audio: AudioStreamPlayer2D
+
+@export var collision_score = 0 # Score gained for getting hit by vehicle
+@export var end_score = 0 # Score gained for reaching end
+
+var is_done: bool = false
 var tween: Tween
 	
 func _ready():
@@ -8,7 +15,36 @@ func _ready():
 	growShrinkSprite()	
 	
 func growShrinkSprite():
-
 	tween.set_loops()
 	tween.tween_property(self, "scale", Vector2(0.2, 0.2), 0.4).as_relative()
 	tween.tween_property(self, "scale", Vector2(-0.2, -0.2), 0.4).as_relative()
+
+func _on_collision_component_body_entered(body):
+	if is_done:
+		return
+	if body.is_in_group("Vehicle"):
+		vehicle_collision()
+
+func vehicle_collision():
+	tween.stop()
+	is_done = true
+	score_comp.add_score(collision_score)
+	stop()
+	collision_audio.play()
+	await collision_audio.finished
+	self.queue_free()
+
+func end_collision():
+	is_done = true
+	score_comp.add_score(end_score)
+
+func _on_collision_component_area_entered(area):
+	if is_done:
+		return
+	if area.is_in_group("End"):
+		end_collision()
+
+func _on_collision_component_area_exited(area):
+	if area.is_in_group("End"):
+		self.queue_free()
+
